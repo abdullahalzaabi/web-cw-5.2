0bcc790efc48db6346edd39383599cf25b0b17d8
abdullahalzaabi <66646611+abdullahalzaabi@users.noreply.github.com>
2020-06-29 22:33:19 +0300

    ...

---
+++ Development/js/scripts.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+const food = prompt("pick a number!! ")
+    if (food = 1) {
+console.log ("pizza !! perfect choice")
+if (food = 2) {
+    console.log ("pasta !! perfect choice")
+    }
+    if (food = 2) {
+        console.log ("burgers !! perfect choice")
+        }
+    
+let n1 = parseInt(prompt( 'enter the first number '));
+let n2 = parseInt(prompt('enter the 2nd Number'));
+let operation =parseInt(prompt( 'choose an operation * or - or or = or /'));
+console.log (n1-n2)
+}
+
+else if (operation = '*') {
+    console.log (n1*n2)
+}
+    else if ( operation = '+') {
+        console.log (n1+n2)
+        }
+        else if (operation = '/') {
+            console.log (n1/n2)
+            }
+
+
+
+
+
+
+
